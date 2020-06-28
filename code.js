d33c4181afeed4b15e5209efc439764b51faef84
MohammedGhafri <eng.m.ghafri@gmail.com>
2020-06-28 19:06:27 +0300

    colors

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 'use strict';
-var a=$(document).innerWidth();
+var a=$(document).innerWidth();// to get the width of the screen
 var boxes=['a','b','c','d','e','f','g','h','i'];
-console.log(a);
-if(a<767){
+// console.log(a);
+if(a<767){// this for the moblie view
 $("#h2").text("mobile");
 $("#p1").text("480px - 767px");
 boxes.forEach((item,idx)=>{
@@ -11,13 +11,13 @@ boxes.forEach((item,idx)=>{
     let c=`url("https://via.placeholder.com/1/${b}")`;
 
 $(`.${item}`).css("background-image",c);})
-}else{
+}else{// for the desktop view
     
     $("#h2").text("desktop:");
     $("#p1").css("margin-left", "61px");
     $("#p1").text("768px - 960px");
     boxes.forEach((item,idx)=>{
-        let b=Math.floor(Math.random()*900000) + 100000;
+        let b=Math.floor(Math.random()*900000) + 100000;// to generate rendom colors number
         console.log(b);
         let c=`url("https://via.placeholder.com/1/${b}")`;
     
