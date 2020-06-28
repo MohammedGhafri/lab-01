ad7eac832e1873df33928ef3e7649f540ed15d39
MohammedGhafri <eng.m.ghafri@gmail.com>
2020-06-28 18:14:55 +0300

    change colors

--- code.js
+++ code.js
@@ -1,13 +1,26 @@
 'use strict';
 var a=$(document).innerWidth();
+var boxes=['a','b','c','d','e','f','g','h','i'];
 console.log(a);
 if(a<767){
 $("#h2").text("mobile");
 $("#p1").text("480px - 767px");
+boxes.forEach((item,idx)=>{
+    let b=Math.floor(Math.random()*900000) + 100000;
+    console.log(b);
+    let c=`url("https://via.placeholder.com/1/${b}")`;
+
+$(`.${item}`).css("background-image",c);})
 }else{
+    
     $("#h2").text("desktop:");
     $("#p1").css("margin-left", "61px");
     $("#p1").text("768px - 960px");
-    $(".a").css("background-image",'url("https://via.placeholder.com/680x75/0000FF")');
+    boxes.forEach((item,idx)=>{
+        let b=Math.floor(Math.random()*900000) + 100000;
+        console.log(b);
+        let c=`url("https://via.placeholder.com/1/${b}")`;
+    
+    $(`.${item}`).css("background-image",c);})
     
 }
